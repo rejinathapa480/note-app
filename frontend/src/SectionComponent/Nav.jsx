02543eb7526f4421067520e41763f67010c03337
rejinathapa480 <rejinathapa707@gmail.com>
2025-02-01 22:47:07 +0545

    implement logout

--- frontend/src/SectionComponent/Nav.jsx
+++ frontend/src/SectionComponent/Nav.jsx
@@ -1,11 +1,21 @@
+import useAuthStore from "../authStore/useAuthStore"
+import Login from "../Pages/Login";
+
 const Nav = () =>{
+  const {logout} = useAuthStore();
+
+//   console.log(logout)
+//     const handleLogout = () =>{
+//       logout();
+//     }
+   
     return(
         <>
-        <div className="py-5 px-30">
-            <ul className="flex  justify-between ">
-                <li className="font-semibold text-3xl">Note</li>
-                <li className="bg-blue-700 px-5 py-1 text-white rounded-sm ">Logout</li>
-            </ul>
+        <div className="">
+            <div className="flex  justify-between ">
+                <h2 className="font-semibold text-3xl">Note</h2>
+                <button className="bg-blue-700 px-5 py-1 text-white rounded-sm cursor-pointer" onClick={() => logout()} >Logout</button>
+            </div>
         </div>
         </>
     )
